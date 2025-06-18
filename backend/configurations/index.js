nguyentienhau/nodejs-shopify-template

Initialize project

backend/configurations/index.js
@@ -1,0 +1,16 @@
+const fs = require("fs");
+const path = require("path");
+
+module.exports = fs
+	.readdirSync(__dirname)
+	.filter(function (fileName = "") {
+		const extensionIndex = fileName.lastIndexOf(".");
+		return fileName !== path.basename(__filename) && extensionIndex > 0 && extensionIndex < fileName.length - 1;
+	})
+	.reduce(function (accumulator = {}, fileName = "") {
+		const extensionIndex = fileName.lastIndexOf(".");
+		const filePath = path.resolve(__dirname, fileName);
+		const name = fileName.slice(0, extensionIndex).toCamelCase();
+		accumulator[`${name}Configurations`] = require(filePath);
+		return accumulator;
+	}, {});

backend/models/index.js
@@ -1,0 +1,25 @@
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
+const configuration = require("../configurations").databaseConfigurations[process.env.NODE_ENV || "development"];
+
+const database = { Sequelize, sequelize: new Sequelize(configuration) };
+
+fs.readdirSync(__dirname)
+	.filter(function (fileName = "") {
+		const extensionIndex = fileName.lastIndexOf(".");
+		return fileName !== path.basename(__filename) && extensionIndex > 0 && extensionIndex < fileName.length - 1;
+	})
+	.forEach(function (fileName = "") {
+		const filePath = path.resolve(__dirname, fileName);
+		const model = require(filePath)(database.sequelize);
+		database[model.name] = model;
+	});
+
+Object.keys(database).forEach(function (modelName = "") {
+	if (database[modelName].associate) {
+		database[modelName].associate(database);
+	}
+});
+
+module.exports = database;

nguyentienhau/nodejs-shopify-application

backend/configurations/index.js
@@ -1,0 +1,16 @@
+const fs = require("fs");
+const path = require("path");
+
+module.exports = fs
+	.readdirSync(__dirname)
+	.filter(function (fileName = "") {
+		const extensionIndex = fileName.lastIndexOf(".");
+		return fileName !== path.basename(__filename) && extensionIndex > 0 && extensionIndex < fileName.length - 1;
+	})
+	.reduce(function (accumulator = {}, fileName = "") {
+		const extensionIndex = fileName.lastIndexOf(".");
+		const filePath = path.resolve(__dirname, fileName);
+		const name = fileName.slice(0, extensionIndex).toPascalCase();
+		accumulator[`${name}Configuration`] = require(filePath);
+		return accumulator;
+	}, {});

backend/models/index.js
@@ -1,0 +1,25 @@
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
+const configuration = require("../configurations").DatabaseConfiguration[process.env.NODE_ENV || "development"];
+
+const database = { Sequelize, sequelize: new Sequelize(configuration) };
+
+fs.readdirSync(__dirname)
+	.filter(function (fileName = "") {
+		const extensionIndex = fileName.lastIndexOf(".");
+		return fileName !== path.basename(__filename) && extensionIndex > 0 && extensionIndex < fileName.length - 1;
+	})
+	.forEach(function (fileName = "") {
+		const filePath = path.resolve(__dirname, fileName);
+		const model = require(filePath)(database.sequelize);
+		database[model.name] = model;
+	});
+
+Object.keys(database).forEach(function (modelName = "") {
+	if (database[modelName].associate) {
+		database[modelName].associate(database);
+	}
+});
+
+module.exports = database;
nguyentienhau/nodejs-shopify-template

app-general: Initialize app general

frontend/components/app-general/layout.jsx
@@ -1,0 +1,32 @@
+import PropTypes from "prop-types";
+import { useState, useEffect } from "react";
+import { useDispatch } from "@frontend/utilities";
+import { AgHelpers } from "@frontend/helpers";
+
+export function Layout({ children = "" }) {
+	const dispatch = useDispatch();
+	const [loading, setLoading] = useState(true);
+
+	async function fetchData() {
+		const [userResponse, shopResponse] = await Promise.all([
+			AgHelpers.request("/ag-user", { type: "read" }),
+			AgHelpers.request("/sa-shop", { type: "read" })
+		]);
+
+		if (userResponse.success && userResponse.data.id && shopResponse.success && shopResponse.data.id) {
+			dispatch("appGeneral.user", userResponse.data);
+			dispatch("shopifyAdmin.shop", shopResponse.data);
+			setLoading(false);
+		}
+	}
+
+	useEffect(function () {
+		fetchData();
+	}, []);
+
+	return loading ? null : children;
+}
+
+Layout.propTypes = {
+	children: PropTypes.node
+};

nguyentienhau/nodejs-shopify-application

frontend/components/app-general/layout.jsx
@@ -1,0 +1,32 @@
+import PropTypes from "prop-types";
+import { useState, useEffect } from "react";
+import { useDispatch } from "@frontend/utilities";
+import { AgHelpers } from "@frontend/helpers";
+
+export function Layout({ children = "" }) {
+	const dispatch = useDispatch();
+	const [loading, setLoading] = useState(true);
+
+	async function fetchData() {
+		const [userResponse, shopResponse] = await Promise.all([
+			AgHelpers.request("/ag-user", { type: "read" }),
+			AgHelpers.request("/sp-shop", { type: "read" })
+		]);
+
+		if (userResponse.success && userResponse.data.id && shopResponse.success && shopResponse.data.id) {
+			dispatch("appGeneral.user", userResponse.data);
+			dispatch("shopify.shop", shopResponse.data);
+			setLoading(false);
+		}
+	}
+
+	useEffect(() => {
+		fetchData();
+	}, []);
+
+	return loading ? null : children;
+}
+
+Layout.propTypes = {
+	children: PropTypes.node
+};